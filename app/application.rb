42a015659ba79589e24454a5cb0d178de5559330
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-12-31 21:39:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.new
-    puts "#{time}\n"
+    resp.write "#{time}\n"
     if time.hour <= 12
       resp.write "Good Morning!"
     else
