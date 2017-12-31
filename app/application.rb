15202d3539996f5f90bf118e479a01f9846b0bb6
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-12-31 21:33:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,8 @@
 
 class Application
   resp = Rack::Response.new
-  time = Time.hour
-  if time < 12
+  time = Time.new
+  if time.hour < 12
     resp.write "Good Morning!"
   else
     resp.write "Good Afternoon"
